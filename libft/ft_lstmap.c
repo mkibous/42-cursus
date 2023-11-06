c8fc2724fcf46800f2d6352163ca1f257d6269fe
Mehdi Kibous <mkibous@e3r3p4.1337.ma>
2023-11-06 14:11:32 +0100

    sss

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mkibous <mkibous@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 10:39:24 by mkibous           #+#    #+#             */
-/*   Updated: 2023/11/06 10:40:43 by mkibous          ###   ########.fr       */
+/*   Updated: 2023/11/06 13:15:29 by mkibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,52 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list *new;
+    t_list *first;
+
+    first = NULL;
+    //ft_lstadd_back(&lst, end);
+    while (lst)
+    {
+        new = ft_lstnew(f(lst->content));
+        if(!new)
+        {
+            ft_lstclear(&first, del);
+            return (NULL);
+        }
+       ft_lstadd_back(&first, new);
+        lst = lst->next;
+    }
+    return (first);
     
 }
+/*void *ft_f (void *s)
+{
+    return("mehdi");
+}
+void ft_free(void *ds)
+{
+    free(ds);
+}
+int main()
+{
+    char str[] = "hello";
+    t_list *h = ft_lstnew(str);
+    h->content = malloc(10);
+    ft_strlcpy(h->content, str, sizeof(str));
+    t_list *n = ft_lstnew(str);
+    n->content = malloc(10);
+    ft_strlcpy(n->content, str, sizeof(str));
+    h->next = n;
+    n->next = NULL;
+    printf("this is the content : %s\n", h->content);
+    printf("this is the content : %s\n", n->content);
+    t_list *new = ft_lstmap(h, ft_f, ft_free);
+    while (new)
+    {
+        printf("%s\n", new->content);
+        new = new->next;
+    }
+    //printf("this is the content : %s\n", n->content);
+    return 0;
+}*/
